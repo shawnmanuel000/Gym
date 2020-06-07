cd209e1e53ffc5bf4ec296dbfa8c2d302779a9db
Shawn Manuel <shawn@DN51s4mk.SUNet>
2020-06-06 21:44:00 -0700

    Adding box2d

--- gym/envs/box2d/__init__.py
+++ gym/envs/box2d/__init__.py
@@ -1,8 +1,8 @@
 try:
     import Box2D
-    from gym.envs.box2d.lunar_lander import LunarLander
-    from gym.envs.box2d.lunar_lander import LunarLanderContinuous
-    from gym.envs.box2d.bipedal_walker import BipedalWalker, BipedalWalkerHardcore
-    from gym.envs.box2d.car_racing import CarRacing
+    from .lunar_lander import LunarLander
+    from .lunar_lander import LunarLanderContinuous
+    from .bipedal_walker import BipedalWalker, BipedalWalkerHardcore
+    from .car_racing import CarRacing
 except ImportError:
     Box2D = None
